21f4f50b204de054a59887aabaab8710b5adc7e0
Sigit Aji P <sigitnj89@gmail.com>
2023-10-14 17:36:37 +0700

    JS Lanjutan Object.create 1.2

--- public/script.js
+++ public/script.js
@@ -16,25 +16,29 @@
 
 //Obj Function Declaration
 
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+const methodeMahasiswa = {
 
-//     mahasiswa.makan = function (porsi) {
-//         this.energi = this.energi + porsi;
-//         console.log(`Halo ${this.nama}, energi mu bertambah ${porsi}, total energi mu = ${this.energi}`);
+    makan: function (porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Halo ${this.nama}, energi mu bertambah ${porsi}, total energi mu = ${this.energi}`);
 
-//     }
+    },
 
-//     mahasiswa.gelud = function(jam) {
-//         this.energi -= jam;
-//         console.log(`Energi mu berkurang ${jam}, total energi mu sekarang ${this.energi}`);
-//     }
-//     return mahasiswa;
-// }
+    gelud: function(jam) {
+        this.energi -= jam;
+        console.log(`Energi mu berkurang ${jam}, total energi mu sekarang ${this.energi}`);
+    }
+}
 
-// let Sigit = Mahasiswa("Sigit", 30);
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodeMahasiswa); 
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+
+    return mahasiswa;
+}
+
+let Sigit = Mahasiswa("Sigit", 30);
 
 
 
@@ -42,19 +46,19 @@
 
 // Constructor function (Keyword "New")
 
-function Mahasiswa(name, energy) {
-    this.name = name;
-    this.energy = energy;
+// function Mahasiswa(name, energy) {
+//     this.name = name;
+//     this.energy = energy;
 
-    this.makan = function(porsi) {
-        this.energy += porsi;
-        console.log(`Hallo ${this.name}, Energy mu bertambah ${porsi}, total energy mu ${this.energy}`);
-    }
+//     this.makan = function(porsi) {
+//         this.energy += porsi;
+//         console.log(`Hallo ${this.name}, Energy mu bertambah ${porsi}, total energy mu ${this.energy}`);
+//     }
 
-    this.gelud = function(jam) {
-        this.energy -= jam;
-        console.log(`Energy mu telah berkurang sebesar ${jam}, total energy mu sekarang ${this.energy}`);
-    }
-}
+//     this.gelud = function(jam) {
+//         this.energy -= jam;
+//         console.log(`Energy mu telah berkurang sebesar ${jam}, total energy mu sekarang ${this.energy}`);
+//     }
+// }
 
-let Sigit = new Mahasiswa("Sigit", 50);
+// let Sigit = new Mahasiswa("Sigit", 50);
