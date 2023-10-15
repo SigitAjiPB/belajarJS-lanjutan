5d095272047cbe3453ff709e6c678dcf4f8615c1
Sigit Aji P <sigitnj89@gmail.com>
2023-10-15 18:50:43 +0700

    JS Lanjutan - Closure 2.2 (immediately invoke function)

--- public/closure.js
+++ public/closure.js
@@ -1,20 +1,44 @@
 // Closure 
 
-function init() {
-    // let nama = "sigit";
-    function tampilNama(nama) {
-        console.log(nama);
-    }
-    return tampilNama;
-}
-let palnggilNama = init();
-palnggilNama('Sigit');
+// function init() {
+//     // let nama = "sigit";
+//     function tampilNama(nama) {
+//         console.log(nama);
+//     }
+//     return tampilNama;
+// }
+// let palnggilNama = init();
+// palnggilNama('Sigit');
 
-function init() {
-    function tampilNama(nama) {
-        console.log(nama);
+// function init() {
+//     function tampilNama(nama) {
+//         console.log(nama);
+//     }
+//     return tampilNama;
+// }
+// let manggilNama = init();
+// manggilNama("Aji Primal"); 
+
+
+
+
+
+// counter menggunakan closure
+// menggunakan immediately invoke function (menjalankan inner function tanpa harus membuat variable atau penampungnya)
+
+
+let add = (function() {
+    let counter = 0;
+    return function() {
+        
+        return ++counter;
     }
-    return tampilNama;
-}
-let manggilNama = init();
-manggilNama("Aji Primal"); 
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
