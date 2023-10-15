33dea7d3e42802b17605dac408f3009c53e5dab2
Sigit Aji P <sigitnj89@gmail.com>
2023-10-15 08:05:26 +0700

    JS lanjutan Prototype 1.3 Part.2 - Class version

--- public/script.js
+++ public/script.js
@@ -43,38 +43,75 @@
 
 
 // Mengubah function declaration => constructor function
-function Mahasiswa(nama, energi, uang, makanan) {
-    // let mahasiswa = Object.create(methodeMahasiswa); 
-    this.nama = nama;
-    this.energi = energi;
-    this.uang = uang;
-    this.makanan = makanan;
+// function Mahasiswa(nama, energi, uang, makanan) {
+//     // let mahasiswa = Object.create(methodeMahasiswa); 
+//     this.nama = nama;
+//     this.energi = energi;
+//     this.uang = uang;
+//     this.makanan = makanan;
 
-    // return mahasiswa;
+//     // return mahasiswa;
+// }
+
+// Mahasiswa.prototype.makan = function(porsi) {
+//     this.energi += porsi * 3;
+//     this.makanan -= porsi;
+//     console.log(`Halo ${this.nama}, kamu memperoleh energi sebesar ${porsi * 3}. Total energi mu sekarang ${this.energi}`);
+// }
+
+// Mahasiswa.prototype.gelud = function(terkuras) {
+//     this.energi -= terkuras * 5;
+//     this.uang += terkuras + terkuras * 3;
+
+//     console.log(`Halo ${this.nama}, energi mu terkuras sebanyak ${terkuras}. Energi mu sekarang adalah ${this.energi}, uang mu bertambah menjadi ${this.uang}`);
+// }
+
+// Mahasiswa.prototype.topUp = function(bayar) {
+//     this.uang -= bayar;
+//     this.makanan += bayar * 1;
+
+//     console.log(`Halo ${this.nama}, kamu telah membeli makanan sebanyak ${bayar}. Sisa uang mu sebanyak ${this.uang}`);
+// }
+
+// let Sigit = new Mahasiswa("Sigit", 50, 200, 0);
+
+
+
+
+
+// Membuat Class menggunakan function destructor
+class Character {
+    constructor (name, energy, cash, food) { 
+        this.name = name;
+        this.energy = energy;
+        this.cash = cash;
+        this.food = food;
+    }
+
+    eat(portion) {
+        this.energy += portion * 5;
+        this.food -= portion * 5;
+        console.log(`Hallo ${this.name} your energy has increased to ${portion}, your energy now is ${this.energy}`);
+    }
+
+    grind(times) {
+        this.energy -= times * 5;
+        this.cash += times * 10;
+        console.log(`Hallo ${this.name}, your energy has decreased by ${times * 5}, your energy now is ${this.energy}, and you recieved ${times * 10} gold`);
+    }
+
+    buyFood(times) {
+        this.cash -= times * 5;
+        this.food += times * 50;
+        console.log(`Hallo ${this.name} you buy ${times * 50} foods, now you have ${this.food} foods in your backpack, and your gold is ${this.cash}`);
+    }
+
+
+
+
 }
 
-Mahasiswa.prototype.makan = function(porsi) {
-    this.energi += porsi * 3;
-    this.makanan -= porsi;
-    console.log(`Halo ${this.nama}, kamu memperoleh energi sebesar ${porsi * 3}. Total energi mu sekarang ${this.energi}`);
-}
-
-Mahasiswa.prototype.gelud = function(terkuras) {
-    this.energi -= terkuras * 5;
-    this.uang += terkuras + terkuras * 3;
-
-    console.log(`Halo ${this.nama}, energi mu terkuras sebanyak ${terkuras}. Energi mu sekarang adalah ${this.energi}, uang mu bertambah menjadi ${this.uang}`);
-}
-
-Mahasiswa.prototype.topUp = function(bayar) {
-    this.uang -= bayar;
-    this.makanan += bayar * 1;
-
-    console.log(`Halo ${this.nama}, kamu telah membeli makanan sebanyak ${bayar}. Sisa uang mu sebanyak ${this.uang}`);
-}
-
-let Sigit = new Mahasiswa("Sigit", 50, 200, 0);
-
+let sigit = new Character("Sigit", 50, 200, 0);
 
 
 
