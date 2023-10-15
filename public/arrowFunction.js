dc9c2a2091ef3fc9ddfb5e47161260a7c0967b74
Sigit Aji P <sigitnj89@gmail.com>
2023-10-15 21:23:35 +0700

    JS lanjutan - Arrow Function (konsep yang dimiliki arrow function)

---
+++ public/arrowFunction.js
@@ -0,0 +1,36 @@
+// mencoba menggunakan arrow function
+
+// Mencari jumlah karakter angka pada array nama
+// let character = ["Sigit", "aji", "Primalbeast"];
+
+// let jumlahHuruf = character.map(nama => nama.length );
+// console.log(jumlahHuruf);
+
+// Mengubah array menjadi object dengan menggunakan arrow function
+// let jumlahHuruf = character.map(nama => ({
+//     nama: nama,
+//     jmlHuruf: nama.length
+// }));
+
+// console.table(jumlahHuruf);
+
+
+
+
+// arrow function (tidak memiliki konsep "this")
+// memnambahkan umur setiap 1 detik
+
+const Character = function() {
+    this.nama = "Sigit";
+    this.umur = 33;
+
+    this.sayHallo = function() {
+        console.log(`Hallo nama saya ${this.nama}, saya ${this.umur} tahun`);
+    }
+
+    // setInterval(()=> {
+    //     console.log(this.umur++)
+    // },1000);
+}
+
+const Sigit = new Character();
